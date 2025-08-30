90c61341ee9e629e12841afc3aaa02b9648f1ae4
Pradyumn Vikram <pradyumn.vik@gmail.com>
2025-08-31 00:35:58 +0530

    I fucked up, now I have fixed it:)

--- localisation.py
+++ localisation.py
@@ -36,11 +36,13 @@ def residuals(point, measured_tdoas, microphones, speed_of_sound):
     return res
 
 def coarse_to_fine_search(measured_tdoas, microphones, speed_of_sound, cube_side,
-                          coarse_res=0.02, fine_search_radius=0.03):
-    # Coarse grid search over entire space
-    x_coarse = np.arange(0, cube_side + coarse_res/2, coarse_res)
-    y_coarse = np.arange(0, cube_side + coarse_res/2, coarse_res)
-    z_coarse = np.arange(0, cube_side + coarse_res/2, coarse_res)
+                          coarse_res=0.05, fine_search_radius=0.04):
+    lower_bound_volume = np.array([0, 0, -2.5]) 
+    upper_bound_volume = np.array([0.1, 0.1, 0.1])  
+
+    x_coarse = np.arange(lower_bound_volume[0], upper_bound_volume[0] + coarse_res/2, coarse_res)
+    y_coarse = np.arange(lower_bound_volume[1], upper_bound_volume[1] + coarse_res/2, coarse_res)
+    z_coarse = np.arange(lower_bound_volume[2], upper_bound_volume[2] + coarse_res/2, coarse_res)
     grid_points = np.array(np.meshgrid(x_coarse, y_coarse, z_coarse)).T.reshape(-1,3)
 
     errors = np.zeros(len(grid_points))
@@ -51,52 +53,53 @@ def coarse_to_fine_search(measured_tdoas, microphones, speed_of_sound, cube_side
     best_index = np.argmin(errors)
     coarse_best_point = grid_points[best_index]
 
-    # Bounds for fine nonlinear least squares optimization
-    lower_bound = np.maximum(coarse_best_point - fine_search_radius, 0)
-    upper_bound = np.minimum(coarse_best_point + fine_search_radius, cube_side)
+    # Set fine bounds around coarse best, clipped to the search volume
+    lower_bound = np.maximum(coarse_best_point - fine_search_radius, lower_bound_volume)
+    upper_bound = np.minimum(coarse_best_point + fine_search_radius, upper_bound_volume)
     bounds = (lower_bound, upper_bound)
 
-    # Nonlinear least squares refinement with bounds
+    # Nonlinear least squares refinement
     result = least_squares(residuals, coarse_best_point, bounds=bounds,
                            args=(measured_tdoas, microphones, speed_of_sound), method='trf')
 
-    return result.x * 100 
+    return result.x * 100  # cm
+
 
 if __name__ == "__main__":
+    '''
+        #path to all the mics or some function to get input from each individual mic will come here
+        #currently it contains the path to the test audio files
+        mics = ["/home/azidozide/projects/GCC-PHAT/Test_Audio/B_R-vocals.mp3",
+                "/home/azidozide/projects/GCC-PHAT/Test_Audio/B_R-vocals.mp3",
+                "/home/azidozide/projects/GCC-PHAT/Test_Audio/B_R-vocals.mp3",
+                "/home/azidozide/projects/GCC-PHAT/Test_Audio/B_R-vocals.mp3"]
 
-    #path to all the mics or some function to get input from each individual mic will come here
-    #currently it contains the path to the test audio files
-    mics = ["/home/azidozide/projects/GCC-PHAT/Test_Audio/B_R-vocals.mp3",
-            "/home/azidozide/projects/GCC-PHAT/Test_Audio/B_R-vocals.mp3",
-            "/home/azidozide/projects/GCC-PHAT/Test_Audio/B_R-vocals.mp3",
-            "/home/azidozide/projects/GCC-PHAT/Test_Audio/B_R-vocals.mp3"]
+        input_tdoas = {}
 
-    input_tdoas = {}
-
-    for i in range(4):
-        for j in range(i, 4):
-            if i!=j:
-                input_tdoas[(i,j)], _ = find_delay_between_mp3s(mics[i], mics[j], max_tau=5.0, analysis_length=30)
+        for i in range(4):
+            for j in range(i, 4):
+                if i!=j:
+                    input_tdoas[(i,j)], _ = find_delay_between_mp3s(mics[i], mics[j], max_tau=5.0, analysis_length=30)
 
 
-    #once we get the mics and set it up the code below will be removed and only estimated_position_cm will run
-    #only dummy variables set here to verify working of functions
-    reference_file = "/home/azidozide/projects/GCC-PHAT/Test_Audio/B_R-vocals.mp3"
-    delayed_file = "/home/azidozide/projects/GCC-PHAT/Test_Audio/B_R-vocals_delayed.mp3"
-    delay, _ = find_delay_between_mp3s(
-            reference_file, 
-            delayed_file, 
-            max_tau=5.0,      
-            analysis_length=30 
-        )
-
+        #once we get the mics and set it up the code below will be removed and only estimated_position_cm will run
+        #only dummy variables set here to verify working of functions
+        reference_file = "/home/azidozide/projects/GCC-PHAT/Test_Audio/B_R-vocals.mp3"
+        delayed_file = "/home/azidozide/projects/GCC-PHAT/Test_Audio/B_R-vocals_delayed.mp3"
+        delay, _ = find_delay_between_mp3s(
+                reference_file, 
+                delayed_file, 
+                max_tau=5.0,      
+                analysis_length=30 
+            )
+    '''
     input_tdoas = {
-        (0,1): 0.000123,
-        (0,2): 0.000111,
-        (0,3): 0.000130,
-        (1,2): 0.000020,
-        (1,3): 0.000025,
-        (2,3): 0.000011
+        (0,1): 0,
+        (0,2): -0.000272,
+        (0,3): -0.000272,
+        (1,2): -0.000272,
+        (1,3): -0.000272,
+        (2,3): 0
     }
     
     estimated_position_cm = coarse_to_fine_search(input_tdoas, microphones, speed_of_sound, cube_side)
